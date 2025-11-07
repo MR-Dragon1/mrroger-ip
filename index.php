34028959bdb9f548dbc8660c806b8eb8d22c621a
MR-Dragon1 <mrrindy4@gmail.com>
2025-11-07 20:25:41 +0700

    update

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 header('Content-Type: application/json');
 
 $valid_ip = "174.138.20.184";
+$valid_pass = md5("langitsenja");
 
 echo json_encode([
     "ip" => $valid_ip,
+    "password" => $valid_pass,
 ]);
